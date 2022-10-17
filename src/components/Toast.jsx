af639bc75ee6762ce4ab88aa02afdb4e41ff7b62
treyer <nemereno_by@mail.ru>
2022-10-17 22:39:30 +0300

    feat: init yarn project, init storybook, create Toast and Toast story basis

---
+++ src/components/Toast.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+
+function Toast({ backgroundColor }) {
+  const styles = { backgroundColor };
+  return <div style={styles}>Hello from Toast!!!</div>;
+}
+
+Toast.propTypes = {
+  backgroundColor: PropTypes.string,
+};
+
+export default Toast;
---
+++ src/stories/Toast.stories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Toast from "../components/Toast";
+
+export default {
+  title: "Components/Toast",
+  component: Toast,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+
+const Template = (args) => <Toast {...args} />;
+
+export const Red = Template.bind({});
+
+Red.args = {
+  backgroundColor: "red",
+};
