3c93f268644c8ee10c314e1d9cb6de3d18cf35a8
treyer <nemereno_by@mail.ru>
2022-10-30 22:15:47 +0300

    feat: add fix options functionality

--- src/helpers/getToastSettings.js
+++ src/helpers/getToastSettings.js
@@ -1,6 +1,13 @@
 import { TOAST_STATE } from '@constants/toastStates'
 import { getDefaultDirections } from '@helpers/getDefaultDirections'
 
+const validOptionKeys = [
+  'lifeTime',
+  'showFrom',
+  'hideTo',
+  'backgroundColor',
+]
+
 const methodSignature =
   'addToast(toastText [, toastHeader [, optionsObject]])'
 
@@ -56,7 +63,7 @@ export const getToastSettings = (
         typeof args[1] === 'string' ? args[1] : null,
       text: args[0],
       options: isObject(args[1])
-        ? { ...defaultOptions, ...args[1] }
+        ? { ...defaultOptions, ...fixOptions(args[1]) }
         : defaultOptions,
     }
   }
@@ -64,11 +71,44 @@ export const getToastSettings = (
     return {
       headerText: args[1],
       text: args[0],
-      options: { ...defaultOptions, ...args[2] },
+      options: {
+        ...defaultOptions,
+        ...fixOptions(args[2]),
+      },
     }
   }
 }
 
 const isObject = value => {
   return value !== null && typeof value === 'object'
+}
+
+const fixOptions = obj => {
+  const wrongKeys = []
+  Object.keys(obj).forEach(key => {
+    if (!validOptionKeys.includes(key)) {
+      wrongKeys.push(key)
+      delete obj[key]
+    }
+  })
+  if (wrongKeys.length > 0) {
+    const text =
+      wrongKeys.length === 1 ? 'option key' : 'options keys'
+    console.error(
+      `Unsupported ${text}: ${arrayToString(
+        wrongKeys,
+      )}. Available keys: ${arrayToString(
+        validOptionKeys,
+      )}`,
+    )
+  }
+  return obj
+}
+
+const arrayToString = arr => {
+  return arr.reduce(
+    (res, el, index) =>
+      (res += index !== 0 ? `, "${el}"` : `"${el}"`),
+    '',
+  )
 }
