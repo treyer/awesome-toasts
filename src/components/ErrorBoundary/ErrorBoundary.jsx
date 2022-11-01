f5c824f870286eb426099d236aff8ed6e9a99892
treyer <nemereno_by@mail.ru>
2022-11-01 15:06:16 +0300

    fix: fix ErrorBoundary

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.error) {
       return (
         <React.Fragment>
           <h3>Something went wrong with Toasts.</h3>
